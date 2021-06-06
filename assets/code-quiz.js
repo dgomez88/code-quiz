ec2903a22650e6d39fc73589203ddf1bcc9eada6
Dario Gomez <dariogomezaguirre@gmail.com>
2021-06-06 17:41:17 -0500

    updated script

--- assets/code-quiz.js
+++ assets/code-quiz.js
@@ -1,31 +1,50 @@
+var count = 60;
+var countDownTimer = document.getElementById("count");
+var quizStartButton = document.getElementById("code-quiz-start-btn");
+console.log(quizStartButton);
 
 var questions = [
-    {
-        question: "Commonly used data type Do Not include:---",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within---",
-        choices: ["quotes", "Curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        question: "Arrays in Javascript can be used to store:---",
-        choices: ["numbers and strings", "others Arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    }
-        {
-        question: "String values must be enclosed within --- when being assigned to variables",
-        choices: ["numbers and strings", "other Arrays", "booleances", "all of the above"],
-        answer: "all of the above"
-    }
-        {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:---",
-        choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
-        answer: "console.log"
-    },
-]
+  {
+    question: "Commonly used data type Do Not include:---",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within---",
+    choices: ["quotes", "Curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question: "Arrays in Javascript can be used to store:---",
+    choices: [
+      "numbers and strings",
+      "others Arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within --- when being assigned to variables",
+    choices: [
+      "numbers and strings",
+      "other Arrays",
+      "booleances",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:---",
+    choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
+    answer: "console.log",
+  },
+];
+
+quizStartButton.addEventListener("click",() {
+
+}
 
 
-fun
